2734c66531872c555c8f59a3cf0ae41bdc20e58c
clevison luiz <clevison_luiz@hotmail.com>
2017-11-05 03:31:12 -0300

    ...

---
+++ React/ex/family.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Menber from './member'
+
+export default props => (
+    <div>
+        <Menber name='João' lastName='Silva' />
+        <Menber name='Paulo' lastName='José' />
+        <Menber name='Carlos' lastName='Cordeiro' />
+    </div>
+)
