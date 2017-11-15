d19da69dad4fe4a58704793529d6a68fc13570e4
clevison luiz <clevison_luiz@hotmail.com>
2017-11-15 03:45:16 -0300

    Alterações 15/11

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+       <PageHeader name='Sobre' small='Nós'></PageHeader>
+
+       <h2>Nossa História</h2>
+            <p> Lorem csacsacsac sac sacsacncjsnhajs cbsajb jcksbajcbsajkcbsajcsa</p>
+            <h2>Missão e Visão</h2>
+            <p>csacnsaiocnsiao cnsaicn icnsaio ncsiocnsaio nciosacnisacn</p>
+            <h2>Imprensa</h2>
+            <p>csnak csai ocsaiocosacsi oajcsio acisacjsiao saicjsaiocjsaoi</p>
+    </div>
+)
